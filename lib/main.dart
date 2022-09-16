ff76fb45de4b24d3aba83b6ee5fb66e90a8cf5f5
Abdullah Harits <harits.abdullah19@gmail.com>
2022-09-16 16:33:40 +0700

    fix issue localization

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:easy_localization/easy_localization.dart';
 import 'package:easy_localization_loader/easy_localization_loader.dart';
 import 'package:flutter/material.dart';
@@ -31,6 +33,9 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   // TODO make easylocalization with flutter_bloc
+  // TODO make page responsive to mobile
+  // TODO update translation file
+  
 
   // This widget is the root of your application.
   @override
@@ -57,7 +62,7 @@ class MyApp extends StatelessWidget {
               elevatedButtonTheme: KTheme.elevatedButtonTheme,
               outlinedButtonTheme: KTheme.outlinedButtonTheme,
             ),
-            home: ResponsiveWrapper.builder(const HomePage(),
+            home: ResponsiveWrapper.builder(HomePage(),
                 maxWidth: 1200,
                 minWidth: 480,
                 defaultScale: true,
@@ -66,7 +71,9 @@ class MyApp extends StatelessWidget {
                   const ResponsiveBreakpoint.autoScale(800, name: TABLET),
                   const ResponsiveBreakpoint.resize(1000, name: DESKTOP),
                 ],
-                background: Container(color: const Color(0xFFF5F5F5))),
+                background: Container(
+                  color: Colors.blue,
+                )),
           );
         },
       ),
