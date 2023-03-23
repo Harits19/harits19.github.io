a3ce900cc110d3200d6c2d42d3b670b79cbeb5bf
Harits19 <Harits19@users.noreply.github.com>
2023-03-23 07:39:09 +0000

    deploy: 5c9dbeca9cab85efbe389323c277287ddcd01531

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,7 +4,7 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "flutter.js": "a85fcf6324d3c4d3ae3be1ae4931e9c5",
-"main.dart.js": "c0d6dcc42e201577f0623cccb8e8b18c",
+"main.dart.js": "3440f4f41687a3118a5e0459b319dc10",
 "canvaskit/profiling/canvaskit.js": "c21852696bc1cc82e8894d851c01921a",
 "canvaskit/profiling/canvaskit.wasm": "371bc4e204443b0d5e774d64a046eb99",
 "canvaskit/canvaskit.js": "97937cb4c2c2073c968525a3e08c86a3",
@@ -53,8 +53,8 @@ const RESOURCES = {
 "assets/packages/fluttericon/lib/fonts/Meteocons.ttf": "8b9c7982496155bb39c67eaf2a243731",
 "assets/FontManifest.json": "e3637cec141ff037b4151edf1c0411cb",
 "assets/NOTICES": "8095a8f61f475dd8dccfe8ce58272fb2",
-"index.html": "6f733bce9c64e24c96be5c07f68546c8",
-"/": "6f733bce9c64e24c96be5c07f68546c8",
+"index.html": "afa229ed965d5a942cd23a7e5097a9b7",
+"/": "afa229ed965d5a942cd23a7e5097a9b7",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
