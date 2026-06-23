6cce4a9b0239140b8a64c3c6783f17d9aa7fbb14
Abdullah Harits <harits.abdullah19@gmail.com>
2026-06-23 13:52:23 +0700

    revert back type

---
+++ src/hooks/use_profile/type.tsx
@@ -0,0 +1,47 @@
+export interface CV {
+  name: string;
+  linkedIn: string;
+  github: string;
+  mailto: string;
+  details: DetailItem[];
+  about_me: string;
+  experience: ExperienceItem[];
+  additional_experiences: ExperienceItem[];
+  education: EducationItem;
+  skills: SkillCategory[];
+  languages: LanguageItem[];
+}
+
+export interface DetailItem {
+  text: string;
+  link?: string;
+}
+
+export interface ExperienceItem {
+  company: string;
+  location?: string;
+  employment_type: string;
+  position: string;
+  start_date: string;
+  end_date: string;
+  responsibilities: string[];
+}
+
+export interface EducationItem {
+  institution: string;
+  location: string;
+  degree: string;
+  gpa: string;
+  start_date: string;
+  end_date: string;
+}
+
+export interface SkillCategory {
+  title: string;
+  tools: string[];
+}
+
+export interface LanguageItem {
+  name: string;
+  level: string;
+}
